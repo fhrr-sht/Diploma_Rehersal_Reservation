ad821d9fc1f5b9cad24c17699170a5e30ca7f64b
fhrr-sht <54956154+fhrr-sht@users.noreply.github.com>
2023-03-25 14:46:37 +0200

    Add City Repository

---
+++ RehersalReservation.DataAccessLayer.Contracts/ICityRepository.cs
@@ -0,0 +1,18 @@
+﻿using RehersalReservation.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehersalReservation.DataAccessLayer.Contracts
+{
+    public interface ICityRepository
+    {
+        Task<List<City>> GetCities();
+        Task DeleteCity(int cityID);
+        Task UpdateCity(City city);
+        Task<City> GetCityByID(int cityID);
+        Task InsertCity(City city);
+    }
+}
---
+++ RehersalReservation.DataAccessLayer/CityRepository.cs
@@ -0,0 +1,90 @@
+﻿using RehersalReservation.DataAccessLayer.Contracts;
+using RehersalReservation.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RehersalReservation.DataAccessLayer
+{
+    public class CityRepository : BaseRepository, ICityRepository
+    {
+        public async Task DeleteCity(int cityID)
+        {
+            SqlParameter[] parameters =
+              {
+                new SqlParameter("@CityID", SqlDbType.Int) { Value = cityID}
+                };
+            await ExecuteProcedure("DeleteCity", parameters);
+        }
+
+        public async Task<List<City>> GetCities()
+        {
+            List<City> cities = new List<City>();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("GetCity", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (SqlDataReader rdr = await cmd.ExecuteReaderAsync())
+                {
+                    while (rdr.Read())
+                    {
+                        City city = new City();
+                        city.CityID = int.Parse(rdr["CityID"].ToString());
+                        city.CityName = rdr["CityName"].ToString();
+                        cities.Add(city);
+                    }
+                }
+            }
+            return cities;
+        }
+
+        public async Task<City> GetCityByID(int cityID)
+        {
+            City city = new City();
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("GetCityByID", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter[] parameters =
+                {
+                new SqlParameter("@CityID", SqlDbType.Int) { Value = cityID}
+                };
+                cmd.Parameters.AddRange(parameters.ToArray());
+                using (SqlDataReader rdr = await cmd.ExecuteReaderAsync())
+                {
+                    while (rdr.Read())
+                    {
+                        city.CityName = rdr["CityName"].ToString();
+                        city.CityID = int.Parse(rdr["CityID"].ToString());             
+                    }
+                }
+            }
+            return city;
+        }
+
+        public async Task InsertCity(City city)
+        {
+            SqlParameter[] parameters =
+               {
+                     new SqlParameter("@CityName", SqlDbType.NVarChar, 50) { Value =  city.CityName}
+                };
+            await ExecuteProcedure("InsertCity", parameters);
+        }
+
+        public async Task UpdateCity(City city)
+        {
+            SqlParameter[] parameters =
+                {
+                    new SqlParameter("@CityName", SqlDbType.NVarChar, 50) { Value =  city.CityName},
+                    new SqlParameter("@CityID", SqlDbType.Int) { Value =  city.CityID},
+                };
+            await ExecuteProcedure("UpdateCity", parameters);
+        }
+    }
+}
