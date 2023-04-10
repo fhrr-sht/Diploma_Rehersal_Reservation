2cc216ad22cf7f77eac88f0ebfdf605e21fbd975
fhrr-sht <54956154+fhrr-sht@users.noreply.github.com>
2023-04-10 21:56:17 +0300

    Web application ctreation

---
+++ RehersalReservation/Models/RehersalSpace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RehersalReservation.Models
+{
+    public class RehersalSpace
+    {
+        public int RehersalSpaseID { get; set; }
+        [StringLength(20, ErrorMessage = "Name length can't be more than 20.")]
+        public string RehersalSpaseName { get; set; }
+        public int CityID { get; set; }
+        public string Adress { get; set; }
+    }
+}
